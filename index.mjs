f29c6de0d0dcee916e1ef1391aa12b7ed4d2bf5d
User <sh.alyyew2019@gmail.com>
2022-06-23 14:25:09 +0500

    first

---
+++ index.mjs
@@ -0,0 +1,44 @@
+import express from 'express';
+import cors from 'cors';
+import fs from 'fs';
+import nodemailer from 'nodemailer';
+
+const app = express();
+app.use(cors());
+app.use('/public', express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const server=app.listen(6425, () => {
+    console.log(`listening on port 6425`);
+});
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+    auth: {
+        user: 'sh.alyyew2019@gmail.com',
+        pass: 'weedavwuttyabvde'
+    }
+});
+
+app.post('/send-mail', (req, res) => {
+    let mailOptions = {
+        from: 'sh.alyyew2019@gmail.com',
+        to: 'info@anykhasap.com',
+        subject: `Ulanyjy ${req.body.username}-dan täze hat geldi`,
+        html: `
+        <h4>Ulanyjy ${req.body.username}, Elektron poçtasy ${req.body.email}</h4>
+        <p>${req.body.text}</p>
+        <h5><i>Anyk hasap sorag-jogap merkezi</i></h5>`
+      };
+
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+          res.sendStatus(500);
+        } else {
+          res.send("Sent");
+        }
+      });
+})
