f4ee2431bca281f9be67b45ee01f2c6d121467df
Shageldi-byte <sh.alyyew2019@gmail.com>
2023-01-27 00:30:13 +0500

    first

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import fs from 'fs';
-import nodemailer from 'nodemailer';
+import cors from "cors";
+import express from "express";
+import fs from "fs";
+import nodemailer from "nodemailer";
 
 const app = express();
 app.use(cors());
@@ -17,20 +17,20 @@ const server=app.listen(6425, () => {
 const transporter = nodemailer.createTransport({
   service: 'gmail',
     auth: {
-        user: 'anykhasapquestioncenter@gmail.com',
-        pass: 'aohiyzsxkdaexwdk'
+        user: 'saglykotyquestioncenter@gmail.com',
+        pass: 'nxtrseyszcdbarmn'
     }
 });
 
 app.post('/send-mail', (req, res) => {
     let mailOptions = {
-        from: 'anykhasapquestioncenter@gmail.com',
-        to: 'info@anykhasap.com',
-        subject: `Ulanyjy ${req.body.username}-dan täze hat geldi`,
+        from: 'saglykotyquestioncenter@gmail.com',
+        to: 'saglykoty@gmail.com',
+        subject: `Ulanyjy ${req.body.username}-den täze hat geldi`,
         html: `
         <h4>Ulanyjy ${req.body.username}, Elektron poçtasy ${req.body.email}</h4>
         <p>${req.body.text}</p>
-        <h5><i>Anyk hasap sorag-jogap merkezi</i></h5>`
+        <h5><i>Saglyk oty sorag-jogap merkezi</i></h5>`
       };
 
       transporter.sendMail(mailOptions, function(error, info){
